623e43d5cd659b4086c52bf8496cd654a03bc593
MatthewLoden <matthewloden@tamu.edu>
2021-07-05 16:59:15 -0500

    Created Program to find missing vlaue within array and code to find duplicate numbers in the array

--- findMissingNumber.cpp
+++ findMissingNumber.cpp
@@ -6,7 +6,21 @@ int missingNumber(int A[],int N){
     //A is of size N-1
     int missingNum;
 
-    return missingNum;
+    int sum = 0;
+    for(int i = 0; i < N-1; i++){
+        sum += A[i];
+        cout<<"inputArray["<<i<<"]: "<<A[i]<<endl;
+    }
+    int calculatedSum = 0;
+    int newArray[N];
+    for(int i = 0; i < N;i++){
+        newArray[i] = i+1;
+        calculatedSum += newArray[i];
+    }
+    //cout<<calculatedSum<<endl;
+    missingNum = calculatedSum - sum;
+    cout<<"The Missing Number is: "<<missingNum<<endl;
+    return 0;
 }
 
 int main(){
@@ -16,4 +30,7 @@ int main(){
 
     int myArray[4] = {1,2,3,5};//N = 5
     int myOtherArray[6] = {1,3,4,5,6,7};//N = 7
+
+    missingNumber(myArray,5);
+    missingNumber(myOtherArray,7);
 }
