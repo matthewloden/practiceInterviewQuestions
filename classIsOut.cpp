a90d3b16b52e2addc065512de089f8126d2c67eb
MatthewLoden <matthewloden@tamu.edu>
2021-07-07 16:53:46 -0500

    Updated in Response to Question Asked

--- classIsOut.cpp
+++ classIsOut.cpp
@@ -2,41 +2,34 @@
 #include <string>
 using namespace std;
 
-class classIsOut
+class carDealship       //the parent function
 {
 private:
     /* data */
-    int idNumber;
 public:
-    classIsOut(int x,string y,bool z,int idnum);
-    int roomNumber;
-    string courseName;
-    bool inSchool;
-};
-
-classIsOut::classIsOut(int x,string y,bool z,int idnum){
-    roomNumber = x;
-    courseName = y;
-    inSchool = z;
-    idNumber = idnum;
-};
-
-//how to change object values??!?
-
-
-void newSchoolYear(int roomNumber,string courseName,bool inSchool){
-    roomNumber = roomNumber % 100;
-    courseName = courseName + "101";
-    if(inSchool == false){
-        inSchool = true;
+    string carBrand;
+    string getCarBrand(){       //both parent and child have the same arguemnts and function call items
+        return "Empty String";
     }
-}
+};
+
+class childCarDealship: public carDealship      //this child class is created and copies the public information in it's parent
+{
+public:
+    string engineType;      //created a string for the data to be stored in main() below
+    string getCarBrand(){
+        return engineType;      //returns the engine type of the car
+    }
+};
 
 int main(){
-    classIsOut Matthew(567,"Math",false,10065);
-    classIsOut James(127,"English",true,40123);
-    classIsOut Heather(637,"Science",true,30567);
-    
-    newSchoolYear(Matthew.roomNumber,Matthew.courseName,Matthew.inSchool);
-    cout<<Matthew.roomNumber<<"  "<<Matthew.idNumber<<endl;
+    carDealship parentCar;      //object initailzed with the parent class
+    parentCar.carBrand = "Toyota";
+    //parentCar.engineType = "Gasoline";        not possible to access this variable b/c child can look up to parent but not the other way around
+    childCarDealship childCar;  //object initalized with the child class 
+    childCar.carBrand = "Honda";
+    childCar.engineType = "Electric";       //assigned the engine type to electric
+
+    cout<<"Parent Class .getCarBrand(): "<<parentCar.getCarBrand()<<endl;    //prints the parent classes member function which should be "an empty string" 
+    cout<<"Child Class .getCarBrand(): "<<childCar.getCarBrand()<<endl;     //prints the child classes member function which should output the engine type
 }
